d198f4093b6f20f95daca85e12fca3b4c985e138
Steven Wilson <scmowns@yahoo.com>
2022-04-26 15:29:01 -0700

    added blank activities

--- app/src/main/java/com/example/veggiebuddy/Recipe.java
+++ app/src/main/java/com/example/veggiebuddy/Recipe.java
@@ -2,20 +2,13 @@ package com.example.veggiebuddy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-
-/*
-    MainActivity will be the Onboarding layout (It will load in the beginning)
-
- */
-
-public class MainActivity extends AppCompatActivity {
+public class Recipe extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_recipe);
     }
 }
