7e4def48fb462d6e5f7b6630734cdd110f73c4ea
Steven Wilson <scmowns@yahoo.com>
2022-04-27 23:36:36 -0700

    changed dish images

--- app/src/main/java/com/example/veggiebuddy/HomeScreen.java
+++ app/src/main/java/com/example/veggiebuddy/HomeScreen.java
@@ -55,6 +55,91 @@ public class HomeScreen extends AppCompatActivity {
 
         ImageView clickableimg1 = findViewById(R.id.clickableimg);
         ImageView clickableimg2 = findViewById(R.id.clickableimg2);
+
+        ImageView trendimg3 = findViewById(R.id.trendimg3);
+        ImageView trendimg1 = findViewById(R.id.trendimg1);
+        ImageView trendimg2 = findViewById(R.id.trendimg2);
+        ImageView imagefqwf = findViewById(R.id.imagefqwf);
+        ImageView secondtrendimg1 = findViewById(R.id.secondtrendimg1);
+        ImageView secondtrendimg2 = findViewById(R.id.secondtrendimg2);
+        ImageView imagefqwfweew = findViewById(R.id.imagefqwfweew);
+        ImageView thirdtrendimg1 = findViewById(R.id.thirdtrendimg1);
+        ImageView thirdtrendimg2 = findViewById(R.id.thirdtrendimg2);
+
+
+        trendimg3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(getApplicationContext(), Recipe.class);
+                startActivity(intent);
+            }
+        });
+        trendimg1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(getApplicationContext(), Recipe.class);
+                startActivity(intent);
+            }
+        });
+        trendimg2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(getApplicationContext(), Recipe.class);
+                startActivity(intent);
+            }
+        });
+        imagefqwf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(getApplicationContext(), Recipe.class);
+                startActivity(intent);
+            }
+        });
+        secondtrendimg1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(getApplicationContext(), Recipe.class);
+                startActivity(intent);
+            }
+        });
+        secondtrendimg2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(getApplicationContext(), Recipe.class);
+                startActivity(intent);
+            }
+        });
+        imagefqwfweew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(getApplicationContext(), Recipe.class);
+                startActivity(intent);
+            }
+        });
+        thirdtrendimg1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(getApplicationContext(), Recipe.class);
+                startActivity(intent);
+            }
+        });
+        thirdtrendimg2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(getApplicationContext(), Recipe.class);
+                startActivity(intent);
+            }
+        });
+
         clickableimg1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -88,8 +173,6 @@ public class HomeScreen extends AppCompatActivity {
                 return false;
             }
         });
-
-
 
 
     }
