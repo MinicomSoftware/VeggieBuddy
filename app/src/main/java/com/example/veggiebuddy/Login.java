8ab02fd97e699c84f905ef0436d92568bee9d443
Fang <37891485+fhuzero@users.noreply.github.com>
2022-04-27 18:27:14 -0700

    feat: add Main, Login, and Profile activities
    * add MainActivity activity
    * add Login activity
    * add Profile activity

--- app/src/main/java/com/example/veggiebuddy/Login.java
+++ app/src/main/java/com/example/veggiebuddy/Login.java
@@ -2,13 +2,21 @@ package com.example.veggiebuddy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class Login extends AppCompatActivity {
+public class Login extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(Login.this, Profile.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/veggiebuddy/Profile.java
+++ app/src/main/java/com/example/veggiebuddy/Profile.java
@@ -1,14 +1,83 @@
 package com.example.veggiebuddy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class Profile extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
-    }
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_profile);
+
+                FragmentManager fm = getSupportFragmentManager();
+
+                ViewStateAdapter sa = new ViewStateAdapter(fm, getLifecycle());
+                final ViewPager2 pa = findViewById(R.id.pager);
+                pa.setAdapter(sa);
+
+                // Up to here, we have working scrollable pages
+
+                final TabLayout tabLayout = findViewById(R.id.tabLayout);
+                tabLayout.addTab(tabLayout.newTab().setText("Favorites"));
+                tabLayout.addTab(tabLayout.newTab().setText("Activity"));
+
+                // Now we have tabs, NOTE: I am hardcoding the order, you'll want to do something smarter
+
+                tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+                        @Override
+                        public void onTabSelected(TabLayout.Tab tab) {
+                                pa.setCurrentItem(tab.getPosition());
+                        }
+
+                        @Override
+                        public void onTabUnselected(TabLayout.Tab tab) {
+
+                        }
+
+                        @Override
+                        public void onTabReselected(TabLayout.Tab tab) {
+
+                        }
+                });
+                pa.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+                        @Override
+                        public void onPageSelected(int position) {
+                                tabLayout.selectTab(tabLayout.getTabAt(position));
+                        }
+                });
+
+                // And now we have tabs that, when clicked, navigate to the correct page
+        }
+        private class ViewStateAdapter extends FragmentStateAdapter {
+
+                public ViewStateAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+                        super(fragmentManager, lifecycle);
+                }
+
+                @NonNull
+                @Override
+                public Fragment createFragment(int position) {
+                        // Hardcoded in this order, you'll want to use lists and make sure the titles match
+                        if (position == 0) {
+                                return new FavoritesFragment();
+                        }
+                        return new ActivityFragment();
+                }
+
+                @Override
+                public int getItemCount() {
+                        // Hardcoded, use lists
+                        return 2;
+                }
+        }
 }
