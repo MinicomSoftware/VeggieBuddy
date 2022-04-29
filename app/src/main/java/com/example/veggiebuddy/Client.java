521313ade9701a8a4b79253583b195211073b23c
Steven Wilson <scmowns@yahoo.com>
2022-04-28 22:48:41 -0700

    finished sub and merged

--- app/src/main/java/com/example/veggiebuddy/Client.java
+++ app/src/main/java/com/example/veggiebuddy/Client.java
@@ -8,6 +8,6 @@ public class Client {
 
 
     public static String SEARCH = "";
-
+    public static String[] SUB_ITEMS = {"1 White Onion", "3 Gingers", "10oz Pasta", "3 Bay Leaves", "12oz White Rice", "13oz Brown Rice", "3oz Chia Seeds"};
 }
 
--- app/src/main/java/com/example/veggiebuddy/Recipe.java
+++ app/src/main/java/com/example/veggiebuddy/Recipe.java
@@ -2,6 +2,7 @@ package com.example.veggiebuddy;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,8 +12,13 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class Recipe extends AppCompatActivity {
+
+    private TextView swappingMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -91,5 +97,115 @@ public class Recipe extends AppCompatActivity {
         webSettings.setUseWideViewPort(true);
 
         youtubeWebView.loadUrl("https://www.youtube.com/embed/PrJH8QFmtFE");
+
+
+        //sub functionality:
+        Button subbutton1 = findViewById(R.id.subbutton1);
+        TextView editable1 = findViewById(R.id.editable1);
+
+        Button subbutton2 = findViewById(R.id.subbutton2);
+        TextView editable2 = findViewById(R.id.editable2);
+
+        Button subbutton3 = findViewById(R.id.subbutton3);
+        TextView editable3 = findViewById(R.id.editable3);
+
+        Button subbutton4 = findViewById(R.id.subbutton4);
+        TextView editable4 = findViewById(R.id.editable4);
+
+        //popup
+        ConstraintLayout layout = findViewById(R.id.popup);
+        TextView submessage = findViewById(R.id.submessage);
+
+        subbutton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                layout.setVisibility(View.VISIBLE);
+                swappingMessage = editable1;
+                submessage.setText("Swap " + editable1.getText() + " with?");
+                setSwapBtnMessages();
+            }
+        });
+
+        subbutton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                layout.setVisibility(View.VISIBLE);
+                swappingMessage = editable2;
+                submessage.setText("Swap " + editable2.getText() + " with?");
+                setSwapBtnMessages();
+            }
+        });
+        subbutton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                layout.setVisibility(View.VISIBLE);
+                swappingMessage = editable3;
+                submessage.setText("Swap " + editable3.getText() + " with?");
+                setSwapBtnMessages();
+            }
+        });
+        subbutton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                layout.setVisibility(View.VISIBLE);
+                swappingMessage = editable4;
+                submessage.setText("Swap " + editable4.getText() + " with?");
+                setSwapBtnMessages();
+            }
+        });
+
+        //sub items fuctionalty:
+        Button subItem1 = findViewById(R.id.subItem1);
+        Button subItem2 = findViewById(R.id.subItem2);
+        Button subItem3 = findViewById(R.id.subItem3);
+        Button canclebtn = findViewById(R.id.canclebtn);
+
+        subItem1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                layout.setVisibility(View.INVISIBLE);
+                swappingMessage.setText(subItem1.getText());
+            }
+        });
+        subItem2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                layout.setVisibility(View.INVISIBLE);
+                swappingMessage.setText(subItem2.getText());
+            }
+        });
+
+        subItem3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                layout.setVisibility(View.INVISIBLE);
+                swappingMessage.setText(subItem3.getText());
+            }
+        });
+
+        canclebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                layout.setVisibility(View.INVISIBLE);
+
+            }
+        });
     }
+
+    private void setSwapBtnMessages() {
+
+        Random rnd = new Random();
+        int ran1 = rnd.nextInt(Client.SUB_ITEMS.length);
+        int ran2 = rnd.nextInt(Client.SUB_ITEMS.length);
+        int ran3 = rnd.nextInt(Client.SUB_ITEMS.length);
+
+        Button subItem1 = findViewById(R.id.subItem1);
+        Button subItem2 = findViewById(R.id.subItem2);
+        Button subItem3 = findViewById(R.id.subItem3);
+
+        subItem1.setText(Client.SUB_ITEMS[ran1]);
+        subItem2.setText(Client.SUB_ITEMS[ran2]);
+        subItem3.setText(Client.SUB_ITEMS[ran3]);
+    }
+
 }
