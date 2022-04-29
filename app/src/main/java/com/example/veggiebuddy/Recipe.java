a86e3557e901f48aed3538bc79dd991532f3ec2c
Steven Wilson <minicom@Minicom-PC.local>
2022-04-28 18:12:25 -0700

    added recipie and youtube video player- needing to add the functionality

--- app/src/main/java/com/example/veggiebuddy/Recipe.java
+++ app/src/main/java/com/example/veggiebuddy/Recipe.java
@@ -1,8 +1,16 @@
 package com.example.veggiebuddy;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class Recipe extends AppCompatActivity {
 
@@ -10,5 +18,78 @@ public class Recipe extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+
+         /*
+        Going to be part of every activity
+         */
+        ActionBar actionBar = getSupportActionBar();        //hides the top bar
+        actionBar.hide();
+        ImageView profile = findViewById(R.id.profile);
+        ImageView home = findViewById(R.id.home);
+        ImageView cart = findViewById(R.id.cart);
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(getApplicationContext(), Profile.class);
+                startActivity(intent);
+            }
+        });
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(getApplicationContext(), HomeScreen.class);
+                startActivity(intent);
+            }
+        });
+        cart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(getApplicationContext(), ShoppingCart.class);
+                startActivity(intent);
+            }
+        });
+        /*
+        End of material to always include in every activity
+         */
+
+        ImageView buttonback = findViewById(R.id.buttonback);
+
+        buttonback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(getApplicationContext(), HomeScreen.class);
+                startActivity(intent);
+            }
+        });
+
+        Button addtocart = findViewById(R.id.addtocart);
+
+        addtocart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addtocart.setEnabled(false);
+                addtocart.setText("Added to the Cart!");
+            }
+        });
+
+        WebView youtubeWebView = findViewById(R.id.youtube);
+
+        youtubeWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return false;
+            }
+        });
+
+        WebSettings webSettings = youtubeWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+
+        youtubeWebView.loadUrl("https://www.youtube.com/embed/PrJH8QFmtFE");
     }
 }
