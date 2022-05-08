305767b2f94b5f7091dc85e1f29cf742e9292e1c
Steven Wilson <scmowns@yahoo.com>
2022-05-08 13:58:16 -0700

    fixed buttons

--- app/src/main/java/com/example/veggiebuddy/ShoppingCart.java
+++ app/src/main/java/com/example/veggiebuddy/ShoppingCart.java
@@ -1,10 +1,12 @@
 package com.example.veggiebuddy;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class ShoppingCart extends AppCompatActivity {
 
@@ -12,6 +14,40 @@ public class ShoppingCart extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_cart);
+
+          /*
+        Going to be part of every activity
+         */
+        ImageView profile = findViewById(R.id.profile);
+        ImageView home = findViewById(R.id.home);
+        ImageView cart = findViewById(R.id.cart);
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(getApplicationContext(), Profile.class);
+                startActivity(intent);
+            }
+        });
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(getApplicationContext(), HomeScreen.class);
+                startActivity(intent);
+            }
+        });
+        cart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(getApplicationContext(), ShoppingCart.class);
+                startActivity(intent);
+            }
+        });
+        /*
+        End of material to always include in every activity
+         */
     }
 
     public void sendMessage(View view) {
